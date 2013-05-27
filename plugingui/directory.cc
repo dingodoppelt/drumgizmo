72662f3eee9ba918b623c5e7ba93c5c3b758d8dc
Bent Bisballe Nyeng <deva@aasimon.org>
2013-05-27 19:16:58 +0200

    Fixed stray '(' in emacs style comment.

--- plugingui/directory.cc
+++ plugingui/directory.cc
@@ -1,4 +1,4 @@
-/* -*- M(de: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
 /***************************************************************************
  *            directory.cc
  *
