1dfc8491f069a8fd7a9c870c3e79c2596199b45a
Bent Bisballe Nyeng <deva@aasimon.org>
2014-06-06 15:48:04 +0200

    Tweak cppunit m4 hack.

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ AC_INIT([actest], [1.0.0])
 AM_PATH_CPPUNIT(1.9.6)
 EOF
 [ -f acinclude.m4 ] && rm acinclude.m4
-autoreconf actest 2>/dev/null || echo "AC_DEFUN([AM_PATH_CPPUNIT],[])" > acinclude.m4
+autoreconf -W error actest 2>/dev/null || echo "AC_DEFUN([AM_PATH_CPPUNIT],[])" > acinclude.m4
 rm -Rf actest
 
 # Now run autoreconf
