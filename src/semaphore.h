8d30bef49182bd17ac266af7e2b969a4a78c577d
André Nusser <andre.nusser@googlemail.com>
2016-02-14 15:52:16 +0100

    Change 'Pracro' to 'DrumGizmo' where necessary.

--- src/semaphore.h
+++ src/semaphore.h
@@ -8,20 +8,20 @@
  ****************************************************************************/
 
 /*
- *  This file is part of Pracro.
+ *  This file is part of DrumGizmo.
  *
- *  Pracro is free software; you can redistribute it and/or modify
+ *  DrumGizmo is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation; either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  Pracro is distributed in the hope that it will be useful,
+ *  DrumGizmo is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Pracro; if not, write to the Free Software
+ *  along with DrumGizmo; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
 #ifndef __PRACRO_SEMAPHORE_H__
