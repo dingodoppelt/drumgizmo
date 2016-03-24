6511ddfdc03dd587ef3f406c660b1ff10fe5bd04
Bent Bisballe Nyeng <deva@aasimon.org>
2016-03-24 13:04:21 +0100

    Also compile linux-vst when testing dist.

--- testdist.sh
+++ testdist.sh
@@ -6,6 +6,13 @@ then
 		exit 1
 fi
 
+if [ "$VST_BASE" == "" ]
+then
+		echo "Set the absolute VST_BASE variable."
+		exit 1
+fi
+
+
 VER=$1
 
 make dist && (
@@ -14,7 +21,8 @@ make dist && (
 		rm -Rf drumgizmo-$VER
 		tar xvzf ../drumgizmo-$VER.tar.gz
 		cd drumgizmo-$VER
-		./configure --enable-lv2 --enable-cli --prefix=/usr
+		./configure --enable-lv2 --enable-cli --enable-vst \
+				--with-vst-sources="$VST_BASE" --prefix=/usr
 		make
 		DESTDIR=$PWD/tst/install make install
 )
