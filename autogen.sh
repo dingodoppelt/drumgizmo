a4bfdce8ed87d728f5644077ddbe94380c6ff3b5
Bent Bisballe Nyeng <deva@aasimon.org>
2016-11-01 20:38:23 +0100

    Switch from cppunit.m4 to pkgconfig (patch from Jin^Eld)

--- autogen.sh
+++ autogen.sh
@@ -11,15 +11,5 @@ then
 		exit 1
 fi
 
-# Check for the existence of cppunit.m4. If missing emulate it.
-mkdir -p actest
-cat << EOF > actest/configure.ac
-AC_INIT([actest], [1.0.0])
-AM_PATH_CPPUNIT(1.9.6)
-EOF
-[ -f acinclude.m4 ] && rm acinclude.m4
-autoreconf -W error actest 2>/dev/null || echo "AC_DEFUN([AM_PATH_CPPUNIT],[echo 'cppunit hack'])" > acinclude.m4
-rm -Rf actest
-
 # Now run autoreconf
 ${AUTORECONF:-autoreconf} -fiv --warnings=no-unsupported
