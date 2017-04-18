f5f63d464ee6abc87587678d0cdfd16ebd4e3d91
Bent Bisballe Nyeng <deva@aasimon.org>
2017-04-18 21:01:17 +0200

    Reinstate WS_VISIBLE for win32 windows.

--- plugingui/nativewindow_win32.cc
+++ plugingui/nativewindow_win32.cc
@@ -367,9 +367,7 @@ NativeWindowWin32::NativeWindowWin32(void* native_window, Window& window)
 
 	m_hwnd = CreateWindowEx(0/*ex_style*/, m_className,
 	                        "DGBasisWidget",
-	                        native_window?
-	                        (WS_CHILD | WS_VISIBLE):
-	                        WS_OVERLAPPEDWINDOW,
+	                       (native_window?WS_CHILD:WS_OVERLAPPEDWINDOW) | WS_VISIBLE,
 	                        0, 0, //window.x(), window.y(),
 	                        1, 1, //window.width(), window.height(),
 	                        parent_window, nullptr,
