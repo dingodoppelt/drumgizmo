b6c321a0a88d6b4a78e8e75954079e89bcfe8491
André Nusser <andre.nusser@googlemail.com>
2017-04-19 22:55:07 +0200

    Fix overflow for std::size_t having 32 bits.

--- plugingui/diskstreamingframecontent.h
+++ plugingui/diskstreamingframecontent.h
@@ -53,7 +53,7 @@ private:
 	void reloadClicked();
 
 	// For now the maximum disk streaming limit is 4GB
-	static constexpr std::size_t max_limit = 1024.0 * 1024.0 * 1024.0 * 4.0;
+	static constexpr std::size_t max_limit = 1024.0 * 1024.0 * 1024.0 * 4.0 - 1;
 
 	Label label_text{this};
 	Label label_size{this};
