4515f8152a6a43092995a9314d7d3f5c973bb8e6
Olivier Humbert <trebmuh@tuxfamily.org>
2020-08-27 16:01:30 +0200

    Make tooltips translatable.

--- plugingui/maintab.cc
+++ plugingui/maintab.cc
@@ -28,72 +28,6 @@
 
 #include <translation.h>
 
-namespace
-{
-
-constexpr char humanizer_tip[] = "\
-The first two knobs influence how DrumGizmo simulates the\n\
-stamina of a physical drummers, ie. the fact that they\n\
-loose power in their strokes when playing fast notes:\n\
-  * pAttack: How quickly the velocity gets reduced when\n\
-     playing fast notes.\n\
-     Lower values result in faster velocity reduction.\n\
-  * pRelease: How quickly the drummer regains the velocity\n\
-     when there are spaces between the notes.\n\
-     Lower values result in faster regain.\n\
-\n\
-The last knob controls the randomization of the sample selection:\n\
-  * pStdDev: The standard-deviation for the sample selection.\n\
-     Higher value makes it more likely that a sample further\n\
-     away from the input velocity will be played.";
-
-constexpr char timing_tip[] = "\
-These three knobs influence how DrumGizmo simulates the tightness\n\
-of the drummer. The drifting is defined as the difference between\n\
-the perfect metronome (defined by the note positions) and the 'internal'\n\
-metronome of the drummer which can then drift back and forth in a\n\
-controlled fashion:\n\
-  * pTightness: For each note how much is the drummer allowed to drift.\n\
-     Higher value make the drummer more tight, ie. drift less.\n\
-  * pTimingRegain: Once the drifted, how fast does the drummer's 'internal'\n\
-     metronome get back in sync with the perfect metronome.\n\
-     Higher values moves the timing back towards perfect faster.\n\
-  * pLaidback: Add or subtract a fixed delay in ms to all notes. This will\n\
-     alter the feel of a beat.\n\
-     Positive values are up-beat, negative values are back on the beat.\n\
-\n\
-NOTE: Enabling timing humanization will introduce a fixed delay into the\n\
-audio stream. So this feature should be disabled when using DrumGizmo in\n\
-a real-time scenario such as live with a MIDI drumkit.";
-
-constexpr char sampleselection_tip[] = "\
-These three knobs influence how DrumGizmo selects\n\
-its samples in the following way:\n\
-  * pClose: The importance given to choosing a sample close\n\
-     to the actual MIDI value (after humanization)\n\
-  * pDiversity: The importance given to choosing samples\n\
-     which haven't been played recently.\n\
-  * pRandom: The amount of randomness added.";
-
-constexpr char visualizer_tip[] = "\
-This graph visualizes the time and velocity offsets of last note played\n\
-according to it's ideal input time and velocity.\n\
-The green lines indicate the ideal time and velocity positions.\n\
-The pink areas indicate the spread of the position and velocity of the\n\
-next note in line. The wider the area the more the note can move in time\n\
-and velocity.";
-
-constexpr char power_tip[] = "\
-This function controls how the input powers are mapped to the powers that\n\
-DrumGizmo uses for selecting samples. You can control the function by dragging\n\
-and dropping the three colorful control points. Additionally, you can either\n\
-use a shelf, which draws a horizontal line from the green and red control\n\
-points to the left/right side. Or you can turn off the shelf, and then the\n\
-function goes through the lower left corner, then the three control points,\n\
-and then the upper right corner, enabling to draw more complicated functions.";
-
-} // end anonymous namespace
-
 namespace GUI
 {
 
@@ -116,6 +50,72 @@ MainTab::MainTab(Widget* parent,
 	, settings_notifier(settings_notifier)
 {
 	layout.setSpacing(0);
+
+	const std::string humanizer_tip = std::string(
+		_("The first two knobs influence how DrumGizmo simulates the\n")) +
+		_("stamina of a physical drummers, ie. the fact that they\n") +
+		_("loose power in their strokes when playing fast notes:\n") +
+		_("  * pAttack: How quickly the velocity gets reduced when\n") +
+		_("     playing fast notes.\n") +
+		_("     Lower values result in faster velocity reduction.\n") +
+		_("  * pRelease: How quickly the drummer regains the velocity\n") +
+		_("     when there are spaces between the notes.\n") +
+		_("     Lower values result in faster regain.\n") +
+		_("\n") +
+		_("The last knob controls the randomization of the sample selection:\n") +
+		_("  * pStdDev: The standard-deviation for the sample selection.\n") +
+		_("     Higher value makes it more likely that a sample further\n") +
+		_("     away from the input velocity will be played.");
+
+	const std::string timing_tip = std::string(
+		_("These three knobs influence how DrumGizmo simulates the tightness\n")) +
+		_("of the drummer.\n") +
+		_("The drifting is defined as the difference between\n") +
+		_("the perfect metronome (defined by the note positions) and the 'internal'\n") +
+		_("metronome of the drummer which can then drift back and forth in a\n") +
+		_("controlled fashion:\n") +
+		_("  * pTightness: For each note how much is the drummer allowed to drift.\n") +
+		_("     Higher value make the drummer more tight, ie. drift less.\n") +
+		_("  * pTimingRegain: Once the drifted, how fast does the drummer's 'internal'\n") +
+		_("     metronome get back in sync with the perfect metronome.\n") +
+		_("     Higher values moves the timing back towards perfect faster.\n") +
+		_("  * pLaidback: Add or subtract a fixed delay in ms to all notes. This will\n") +
+		_("     alter the feel of a beat.\n") +
+		_("     Positive values are up-beat, negative values are back on the beat.\n") +
+		_("\n") +
+		_("NOTE: Enabling timing humanization will introduce a fixed delay into the\n") +
+		_("audio stream.\n") +
+		_("So this feature should be disabled when using DrumGizmo in\n") +
+		_("a real-time scenario such as live with a MIDI drumkit.");
+
+	const std::string sampleselection_tip = std::string(
+		_("These three knobs influence how DrumGizmo selects\n")) +
+		_("its samples in the following way:\n") +
+		_("  * pClose: The importance given to choosing a sample close\n") +
+		_("     to the actual MIDI value (after humanization)\n") +
+		_("  * pDiversity: The importance given to choosing samples\n") +
+		_("     which haven't been played recently.\n") +
+		_("  * pRandom: The amount of randomness added.");
+
+	const std::string visualizer_tip = std::string(
+		_("This graph visualizes the time and velocity offsets of last note\n")) +
+		_("played according to it's ideal input time and velocity.\n") +
+		_("The green lines indicate the ideal time and velocity positions.\n") +
+		_("The pink areas indicate the spread of the position and velocity\n") +
+		_("of the next note in line.\n") +
+		_("The wider the area the more the note can move in time and velocity.");
+
+	const std::string power_tip = std::string(
+		_("This function controls how the input powers are mapped to the powers\n")) +
+		_("that DrumGizmo uses for selecting samples.\n") +
+		_("You can control the function by dragging and dropping the\n") +
+		_("three colorful control points.\n") +
+		_("Additionally, you can either use a shelf, which draws a horizontal line\n") +
+		_("from the green and red control points to the left/right side.\n") +
+		_("Or you can turn off the shelf, and then the function goes through\n") +
+		_("the lower left corner, then the three control points, and then\n") +
+		_("the upper right corner, enabling to draw more complicated functions.");
+
 	layout.setResizeChildren(true);
 
 	add(_("Drumkit"), drumkit_frame, drumkitframe_content, 12, 0);
